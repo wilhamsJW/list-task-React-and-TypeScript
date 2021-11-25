3e9aa35de5315f951a752fc03b4489e617a304cf
wilhamsJW <wilhamsjunior@gmail.com>
2021-11-25 20:04:58 -0300

    styling and create componentes

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+import * as C from './App.styles'
+
+import { Item } from './types/Item'
+
+import { ListItem } from './components/ListItem'
+
+import { AddArea } from './components/AddArea'
+
+const App = () => {
+
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Estudar TypeScript', done: true },
+    { id: 2, name: 'Estudar React', done: false },
+    { id: 3, name: 'Estudar NodeJS', done: true }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <C.Container>
+      <C.Area>
+        <C.Header>To Do List</C.Header>
+
+          {/* Adding new Task */}
+          <AddArea />  
+
+          {list.map((item, i) => (
+            <ListItem key={i} item={item} />
+          ))}
+
+      </C.Area>
+    </C.Container>
+  )
 }
 
 export default App;
---
+++ todo/src/components/ListItem/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+type ContainerProps = {
+    done: boolean;
+}
+
+export const Container = styled.div(({ done }: ContainerProps) => ( // esse ContainerProps quer dizer que minha props está usando o typre ContainerProps
+    // no typescript ao usar uma props temos q informar o type dela
+    `
+    display: flex;
+    background-color: #20212C;
+    padding: 10px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+    align-items: center;
+
+    input {
+        width: 20px;
+        height: 20px;
+        margin-right: 15px
+    }
+
+    label {
+        color: #ccc;
+        text-decoration: ${done ? 'line-through' : 'initial'};
+    }
+`
+));
